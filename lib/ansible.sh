8e573f1bff1176e872b09946817df3314abd2183
Ivan Shumkov <ivan@shumkov.ru>
2018-08-24 15:47:13 +0300

    Die with error if ansible config is not found

--- lib/ansible.sh
+++ lib/ansible.sh
@@ -9,14 +9,13 @@ function run_ansible_playbook() {
         print_error "Ansible inventory file not found. Please read README.md how to create infrastructure with Terraform"
     fi
 
-    ANSIBLE_CONFIG_PATH_ARG=""
     if [ -f "../$ANSIBLE_CONFIG_PATH" ]; then
-        ANSIBLE_CONFIG_PATH_ARG="-e @../$ANSIBLE_CONFIG_PATH";
+        print_error "Ansible network config '$ANSIBLE_CONFIG_PATH' is not found. Please read README.md how to configure networks"
     fi
 
     ansible-playbook --private-key="$PRIVATE_KEY_PATH" \
                      -i "../$INVENTORY_FILE" \
-                     ${ANSIBLE_CONFIG_PATH_ARG} \
+                     -e "@../$ANSIBLE_CONFIG_PATH" \
                      -e "dash_network=$NETWORK" \
                      -e "dash_devnet_name=$NETWORK_DEVNET_NAME" \
                      ${ANSIBLE_ARGUMENTS} \
