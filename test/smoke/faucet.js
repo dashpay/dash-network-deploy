8353c9ee04b798981710ae75af9442a0bbbe9cc6
ABVgedeika <andrei.baranouski@yahoo.com>
2018-09-19 17:12:57 +0300

    faucet test improve

--- test/smoke/faucet.js
+++ test/smoke/faucet.js
@@ -1,38 +1,17 @@
-const SSH = require('simple-ssh');
-const fs = require('fs');
-const utils = require('../testUtils');
+const fetch = require('node-fetch');
 
 const getNetworkConfig = require('../../lib/test/getNetworkConfig');
 
 const networkConfig = getNetworkConfig();
 
-var jayson = require('jayson/promise');
-
 describe('Faucet', () => {
   networkConfig.inventory.web.hosts.forEach((nodeName) => {
-    it(`should respond positive balance via insight and cli: ${nodeName}`,
+    it(`should respond positive balance: ${nodeName}`,
       async () => {
-        // const ssh = new SSH({
-        //   host: networkConfig.inventory._meta.hostvars[nodeName].public_ip,
-        //   user: 'ubuntu',
-        //   key: fs.readFileSync(`${process.env.PRIVATE_KEY_PATH}`.replace('~', `${process.env.HOME}`)),
-        // });
-        // let faucetBalance = await utils.echo(ssh, "curl http://127.0.0.1/ | grep 'Faucet balance'");
-        // faucetBalance = parseInt(faucetBalance.replace('<p>Faucet balance:', '').replace('</p>', '').trim());
-        // expect(faucetBalance).to.be.an('number').above(0);
-        var client = jayson.client.http({
-          host: '34.222.89.42',
-          path: '/rpc.php',
-          auth: 'dashrpc:password',
-          headers: {
-            'Accept-Language': 'en-EN',
-          }
-        });
-        var reqs = [client.request('getFunds', []),
-        ];
-        await Promise.all(reqs).then(function(responses) {
-          console.log(responses[0].result);
-        });
+        const faucetBalance = await fetch(`http://${networkConfig.inventory._meta.hostvars[nodeName].public_ip}/`).then(res => res.text());
+        const regex = 'Faucet balance: ([0-9,.]+)';
+        const result = faucetBalance.match(regex);
+        expect(parseFloat(parseFloat(result[1].replace(/,/g, '')))).to.be.an('number').above(0);
       });
   });
 });
