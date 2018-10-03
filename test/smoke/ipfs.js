1c0e6c5eedcc13ef4406ce8bd9d13065728425b4
Ivan Shumkov <ivan@shumkov.ru>
2018-10-03 14:25:06 +0300

    Refactor IPFS test

--- test/smoke/ipfs.js
+++ test/smoke/ipfs.js
@@ -6,46 +6,37 @@ const getNetworkConfig = require('../../lib/test/getNetworkConfig');
 const { variables, inventory } = getNetworkConfig();
 
 describe('IPFS', () => {
-  const allPeers = new Set();
-  for (const hostName of inventory.masternodes.hosts) {
-    describe(hostName, () => {
-      let ipfsApi;
+  it('all nodes should be interconnected', async function it() {
+    if (!variables.evo_services) {
+      this.skip('Evolution services are not enabled');
+      return;
+    }
 
-      beforeEach(() => {
-        // eslint-disable-next-line no-underscore-dangle
-        ipfsApi = new IpfsAPI(inventory._meta.hostvars[hostName].public_ip, '5001', { protocol: 'http' });
-      });
+    const masterNodeHostNames = inventory.masternodes.hosts;
 
-      it('should be interconnected', async function it() {
-        if (!variables.evo_services) {
-          this.skip('Evolution services are not enabled');
-          return;
-        }
+    this.slow(masterNodeHostNames.length * 1500);
+    this.timeout(masterNodeHostNames.length * 2500);
 
-        this.slow(1500);
-
-        const peers = await ipfsApi.swarm.peers();
-        for (const peer of peers) {
-          const ip = multiaddr(peer.addr).nodeAddress().address;
-          allPeers.add(ip);
-        }
-        expect(peers.length).to.be.above(1, 'no peers for node');
-      });
+    // eslint-disable-next-line arrow-body-style
+    const masterNodeHosts = masterNodeHostNames.map((hostName) => {
+      // eslint-disable-next-line no-underscore-dangle
+      return inventory._meta.hostvars[hostName].public_ip;
     });
-  }
-  describe('IPFS peers', () => {
-    it('should all masternodes be in peers', async function it() {
-      if (!variables.evo_services) {
-        this.skip('Evolution services are not enabled');
-        return;
-      }
 
-      const masterNodes = [];
-      for (const hostName of inventory.masternodes.hosts) {
-        // eslint-disable-next-line no-underscore-dangle
-        masterNodes.push(inventory._meta.hostvars[hostName].private_ip);
+    const allPeers = new Set();
+    for (const masterNodeHost of masterNodeHosts) {
+      const ipfsApi = new IpfsAPI(masterNodeHost, '5001', { protocol: 'http' });
+
+      const peers = await ipfsApi.swarm.peers();
+
+      expect(peers.length).to.be.above(1, 'node is not connected');
+
+      for (const peer of peers) {
+        const { address } = multiaddr(peer.addr).nodeAddress();
+        allPeers.add(address);
       }
-      expect(Array.from(allPeers).sort()).to.deep.equal(masterNodes.sort());
-    });
+    }
+
+    expect(Array.from(allPeers).sort()).to.deep.equal(masterNodeHosts.sort());
   });
 });
