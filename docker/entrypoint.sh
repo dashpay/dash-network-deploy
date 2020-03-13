fdf580631f52645df6671ad9111d41a71e63f59c
Ivan Shumkov <ivan@shumkov.ru>
2020-03-13 19:17:49 +0300

    fix: add generate command to Docker image entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-scripts=("deploy destroy test list logs dash-cli")
+scripts=("deploy destroy test list logs dash-cli generate")
 
 if [[ " ${scripts[@]} " =~ " ${1} " ]]; then
     script=$1
