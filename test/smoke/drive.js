73682f257daa753803794aa59aa45911ced77fe5
Ivan Shumkov <ivan@shumkov.ru>
2020-04-01 16:55:13 +0300

    chore: remove machine (#145)

--- test/smoke/drive.js
+++ test/smoke/drive.js
@@ -1,33 +1,45 @@
-const { client: jaysonClient } = require('jayson/promise');
+const net = require('net');
 
 const getNetworkConfig = require('../../lib/test/getNetworkConfig');
 
 const { variables, inventory } = getNetworkConfig();
 
+async function sendEcho(ip) {
+  const echoRequestBytes = Buffer.from('0e12050a03010203', 'hex');
+
+  return new Promise((resolve, reject) => {
+    const client = net.connect(26658, ip);
+
+    client.on('connect', () => {
+      client.write(echoRequestBytes);
+    });
+
+    client.on('data', () => {
+      client.destroy();
+
+      resolve();
+    });
+
+    client.on('error', reject);
+
+    setTimeout(() => {
+      reject(new Error('Can\'t connect to ABCI port: timeout.'));
+    }, 2000);
+  });
+}
+
 describe('Drive', () => {
   for (const hostName of inventory.masternodes.hosts) {
     describe(hostName, () => {
-      let driveClient;
-
-      beforeEach(() => {
-        driveClient = jaysonClient.http({
-          // eslint-disable-next-line no-underscore-dangle
-          host: inventory._meta.hostvars[hostName].public_ip,
-          port: 6000,
-        });
-      });
-
-      it('should respond via JSON RPC', async function it() {
+      it('should listen for ABCI connection', async function it() {
         if (!variables.evo_services) {
           this.skip('Evolution services are not enabled');
+
           return;
         }
 
-        this.slow(500);
-
-        const { error } = await driveClient.request('fetchContract', { contractId: 'wrong' });
-
-        expect(error.code).to.equal(-32602);
+        // eslint-disable-next-line no-underscore-dangle
+        await sendEcho(inventory._meta.hostvars[hostName].public_ip);
       });
     });
   }
