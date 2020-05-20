3da53ddb985651097719bc516c1b7ddea3967be7
Ivan Borovkov <555475+v2nek@users.noreply.github.com>
2020-05-20 18:24:32 +0200

    feat: monitoring alarms to slack (#158)

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -109,6 +109,11 @@ variable "main_domain" {
   default     = ""
 }
 
+variable "montitoring_sns_arn" {
+  description = "ARN of SNS topic that will receive notifications from monitoring"
+  default     = ""
+}
+
 variable "public_network_name" {
   description = "Name of the network that will be used to create dns records"
   default     = ""
