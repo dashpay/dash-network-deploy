3184a383ae532785e51b6dfec4220f1900cd2464
Daniel <daniel@ktechmidas.net>
2021-12-11 21:47:49 +0800

    add DNS record

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -153,6 +153,16 @@ resource "aws_route53_record" "logs" {
   count = length(var.main_domain) > 1 ? 1 : 0
 }
 
+resource "aws_route53_record" "metrics" {
+  zone_id = data.aws_route53_zone.main_domain[count.index].zone_id
+  name    = "metrics.${var.public_network_name}.${var.main_domain}"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.metrics[count.index].public_ip]
+
+  count = length(var.main_domain) > 1 ? 1 : 0
+}
+
 locals {
   dns_record_length = 10 // recommended number of hosts per A record. Other way there might be problems with resolving of seeds in some regions.
 }
