7dc765882e64b90024c5935c13789a8d4c75bdec
Ivan Shumkov <ivan@shumkov.ru>
2023-03-29 12:52:10 +0800

    refactor: drive smoke test

--- test/smoke/drive.js
+++ test/smoke/drive.js
@@ -1,87 +1,58 @@
-const net = require('net');
-
 const getNetworkConfig = require('../../lib/test/getNetworkConfig');
 
-const { inventory } = getNetworkConfig();
+const { inventory, variables } = getNetworkConfig();
 
 const { getDocker, execCommand, getContainerId } = require('../../lib/test/docker');
 
-const seedHosts = inventory.seed_nodes.hosts;
-const masternodeHosts = inventory.masternodes.hosts;
-
-async function sendEcho(ip) {
-  const echoRequestBytes = Buffer.from('0a0a080a0668656c6c6f21', 'hex');
-
-  return new Promise((resolve, reject) => {
-    const client = net.connect(26658, ip);
-
-    client.on('connect', () => {
-      client.write(echoRequestBytes);
-    });
-
-    client.on('data', () => {
-      client.destroy();
-
-      resolve();
-    });
-
-    client.on('error', reject);
-
-    setTimeout(() => {
-      reject(new Error('Can\'t connect to ABCI port: timeout.'));
-    }, 2000);
-  });
-}
-
 describe('Drive', () => {
-  describe('All nodes', () => {
-    const echoInfo = {};
+  describe('HP masternodes', () => {
     const statusInfo = {};
+    const statusError = {};
 
-    before('Collect echo info from Drive', async () => {
-      const promises = [];
-
-      for (const hostName of seedHosts) {
-        const requestEchoPromise = sendEcho(inventory.meta.hostvars[hostName].public_ip)
-          .then(() => {
-            echoInfo[hostName] = true;
-          })
-          .catch((e) => {
-            echoInfo[hostName] = e;
-          });
-
-        promises.push(requestEchoPromise);
+    before('Collect echo info from Drive', async function before() {
+      if (variables.dashmate_platform_enable === false) {
+        this.skip('platform is disabled for this network');
       }
 
       const statusPromises = inventory.hp_masternodes.hosts.map(async (hostName) => {
-        const docker = await getDocker(`http://${inventory.meta.hostvars[hostName].public_ip}`);
+        let docker;
+        try {
+          docker = await getDocker(`http://${inventory.meta.hostvars[hostName].public_ip}`);
+        } catch (e) {
+          statusError[hostName] = e;
 
-        const containerId = await getContainerId(docker, 'dashmate_helper');
+          throw e;
+        }
+
+        let containerId;
+        try {
+          containerId = await getContainerId(docker, 'dashmate_helper');
+        } catch (e) {
+          statusError[hostName] = e;
+
+          throw e;
+        }
 
         try {
           statusInfo[hostName] = await execCommand(docker, containerId,
             ['yarn', 'workspace', 'dashmate', 'dashmate', 'status', 'platform', '--format=json']);
         } catch (e) {
-          console.log('Failed to fetch status on node ' + hostName);
+          statusError[hostName] = e;
+
+          throw e;
         }
       });
 
-      return Promise.all([...promises, ...statusPromises]).catch(console.error);
+      return Promise.all(statusPromises).catch(() => Promise.resolve());
     });
-
-    for (const hostName of seedHosts) {
-      describe(hostName, () => {
-        it('should listen for ABCI connection', () => {
-          if (echoInfo[hostName] !== true) {
-            expect.fail(echoInfo[hostName]);
-          }
-        });
-      });
-    }
 
     for (const hostName of inventory.hp_masternodes.hosts) {
       describe(hostName, () => {
-        it('drive status should be green', () => {
+        it('drive status should be running and responding', () => {
+          if (statusError[hostName]) {
+            expect.fail(statusError[hostName]);
+          }
+
           const { drive } = statusInfo[hostName];
 
           expect(drive.dockerStatus).to.be.equal('running');
