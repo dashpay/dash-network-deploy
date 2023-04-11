ffc573aa80fedd86204978e5e1ef67b37acf8ffd
Ivan Shumkov <ivan@shumkov.ru>
2023-04-11 20:54:18 +0800

    fix(smoke): tenderdash test doesn't report chain halt

--- test/smoke/tenderdash.js
+++ test/smoke/tenderdash.js
@@ -1,53 +1,13 @@
-const { client: jaysonClient } = require('jayson/promise');
-
 const getNetworkConfig = require('../../lib/test/getNetworkConfig');
 const { createDocker, execCommand, getContainerId } = require('../../lib/test/docker');
 
 const { variables, inventory, network } = getNetworkConfig();
 
-const ansibleHosts = inventory.seed_nodes.hosts;
 const dashmateHosts = inventory.hp_masternodes.hosts;
 
-function requestTendermint(host, port, path) {
-  const tendermintClient = jaysonClient.http({
-    host,
-    port,
-  });
-
-  return new Promise((resolve, reject) => {
-    tendermintClient.on('http error', (error) => {
-      reject(error);
-    });
-
-    const timeout = setTimeout(() => {
-      reject(new Error(`timeout connecting to ${host}:${port}`));
-    }, 10000); // 10s timeout
-
-    tendermintClient.on('http response', () => {
-      clearTimeout(timeout);
-    });
-
-    tendermintClient.request(path, {})
-      .then(({ result, error }) => {
-        if (error) {
-          reject(new Error(error));
-
-          return;
-        }
-
-        resolve(result);
-      })
-      .catch((e) => {
-        reject(e);
-      });
-  });
-}
-
-describe('Tenderdash', () => {
+describe.only('Tenderdash', () => {
   const tenderdashStatuses = {};
   const errors = {};
-
-  const blockHashes = {};
 
   describe('HP and seed nodes', () => {
     before('Collect tenderdash info', function collect() {
@@ -55,53 +15,7 @@ describe('Tenderdash', () => {
         this.skip('platform is disabled for this network');
       }
 
-      const promises = [];
-
-      for (const hostName of ansibleHosts) {
-        const requestTendermintStatusPromise = requestTendermint(
-          // eslint-disable-next-line no-underscore-dangle
-          inventory._meta.hostvars[hostName].public_ip,
-          variables.tendermint_rpc_port,
-          'status',
-        ).then((result) => {
-          if (!tenderdashStatuses[hostName]) {
-            tenderdashStatuses[hostName] = {};
-          }
-
-          tenderdashStatuses[hostName] = {
-            network: result.node_info.network,
-            moniker: result.node_info.moniker,
-            lastBlockHeight: result.sync_info.latest_block_height,
-            lastBlockHash: result.sync_info.latest_block_hash,
-            ...tenderdashStatuses[hostName],
-          };
-        }).catch((error) => {
-          errors[hostName] = error;
-        });
-
-        const requestTendermintNetInfoPromise = requestTendermint(
-          // eslint-disable-next-line no-underscore-dangle
-          inventory._meta.hostvars[hostName].public_ip,
-          variables.tendermint_rpc_port,
-          'net_info',
-        ).then((result) => {
-          if (!tenderdashStatuses[hostName]) {
-            tenderdashStatuses[hostName] = {};
-          }
-
-          tenderdashStatuses[hostName] = {
-            peers: result.n_peers,
-            listening: result.listening,
-            ...tenderdashStatuses[hostName],
-          };
-        }).catch((error) => {
-          errors[hostName] = error;
-        });
-
-        promises.push(requestTendermintStatusPromise, requestTendermintNetInfoPromise);
-      }
-
-      const dashmatePromises = dashmateHosts.map(async (hostName) => {
+      const promises = dashmateHosts.map(async (hostName) => {
         const docker = createDocker(inventory.meta.hostvars[hostName].public_ip);
 
         let containerId;
@@ -109,46 +23,46 @@ describe('Tenderdash', () => {
           containerId = await getContainerId(docker, 'dashmate_helper');
         } catch (e) {
           errors[hostName] = e;
+        }
 
-          throw e;
+        if (!containerId) {
+          return;
         }
 
         try {
-          const result = await execCommand(docker, containerId,
-            ['yarn', 'workspace', 'dashmate', 'dashmate', 'status', 'platform', '--format=json']);
+          const { result, error } = await execCommand(
+            docker,
+            containerId,
+            [
+              'curl',
+              '--silent',
+              '-X',
+              'POST',
+              '-H',
+              'Content-Type: application/json',
+              '-d',
+              '{"jsonrpc":"2.0","id":"id","method":"status platform","params": {"format": "json"}}',
+              'localhost:9000',
+            ],
+          );
 
-          tenderdashStatuses[hostName] = result.tenderdash;
+          if (error) {
+            // noinspection ExceptionCaughtLocallyJS
+            throw new Error(error.message);
+          }
+
+          const status = JSON.parse(result);
+
+          tenderdashStatuses[hostName] = status.tenderdash;
         } catch (e) {
           errors[hostName] = e;
-
-          throw e;
         }
       });
 
-      return Promise.all(promises.concat(dashmatePromises)).catch(() => Promise.resolve());
+      return Promise.all(promises).catch(() => Promise.resolve());
     });
 
-    before('Evaluate block hashes', () => {
-      for (const hostName of ansibleHosts.concat(dashmateHosts)) {
-        if (!tenderdashStatuses[hostName]) {
-          // eslint-disable-next-line no-continue
-          continue;
-        }
-
-        const {
-          tenderdash: {
-            lastBlockHeight: blockHeight,
-            lastBlockHash: blockHash,
-          },
-        } = tenderdashStatuses[hostName];
-
-        if (!blockHashes[blockHeight]) {
-          blockHashes[blockHeight] = blockHash;
-        }
-      }
-    });
-
-    for (const hostName of dashmateHosts.concat(ansibleHosts)) {
+    for (const hostName of dashmateHosts) {
       // eslint-disable-next-line no-loop-func
       describe(hostName, () => {
         it('should be connected to the network', () => {
@@ -192,21 +106,9 @@ describe('Tenderdash', () => {
             expect.fail('can\'t get tenderdash status');
           }
 
-          const {
-            lastBlockHash: blockHash,
-            lastBlockHeight: blockHeight,
-          } = tenderdashStatuses[hostName];
+          const latestBlockTime = new Date(tenderdashStatuses[hostName].latestBlockTime);
 
-          expect(blockHashes[blockHeight]).to.be.equal(blockHash);
-
-          const blocksCounts = Object.keys(blockHashes).map((c) => parseInt(c, 10));
-          const maxBlocksCount = Math.max(...blocksCounts);
-
-          const blocksCountDiff = maxBlocksCount - blockHeight;
-
-          if (blocksCountDiff > 3) {
-            expect.fail(`${blocksCountDiff} blocks behind`);
-          }
+          expect(latestBlockTime.getTime()).to.be.closeTo(Date.now(), 30 * 60 * 1000);
         });
       });
     }
