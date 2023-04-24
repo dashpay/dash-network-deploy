dad9e2a8d0b40f87111104cec69acade1c116a10
strophy <leon.white@dash.org>
2023-04-24 07:46:48 +0000

    chore: bump terraform versions

--- terraform/aws/versions.tf
+++ terraform/aws/versions.tf
@@ -1,14 +1,14 @@
 
 terraform {
-  required_version = "~> 1.4.4"
+  required_version = "~> 1.4.5"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.55"
+      version = "~> 4.64.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.4.3"
+      version = "~> 3.5.1"
     }
   }
 }
