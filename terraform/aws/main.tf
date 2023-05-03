2abe6ff3012dad75300d06d15c408655b3e55ff4
ktechmidas <daniel@ktechmidas.net>
2023-05-03 13:32:33 +0000

    Chore: Ensure seeders only return HPMNs

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -238,22 +238,16 @@ locals {
 
 resource "random_shuffle" "dns_ips" {
   input = concat(
-    aws_instance.masternode_amd.*.public_ip,
-    aws_instance.masternode_arm.*.public_ip,
     var.create_eip ? aws_eip.hpmn_arm_eip.*.public_ip : [],
     var.create_eip ? aws_eip.hpmn_amd_eip.*.public_ip : []
   )
   result_count = length(
     concat(
-      aws_instance.masternode_amd.*.public_ip,
-      aws_instance.masternode_arm.*.public_ip,
       var.create_eip ? aws_eip.hpmn_arm_eip.*.public_ip : [],
       var.create_eip ? aws_eip.hpmn_amd_eip.*.public_ip : []
     )
   ) > local.dns_record_length ? local.dns_record_length : length(
     concat(
-      aws_instance.masternode_amd.*.public_ip,
-      aws_instance.masternode_arm.*.public_ip,
       var.create_eip ? aws_eip.hpmn_arm_eip.*.public_ip : [],
       var.create_eip ? aws_eip.hpmn_amd_eip.*.public_ip : []
     )
