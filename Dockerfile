34b18fd01feee095a152ae408ff9cb4fc9be06c1
strophy <32928115+strophy@users.noreply.github.com>
2023-06-27 13:52:01 +1000

    Revert "chore: merge master into v0.25-dev"

--- Dockerfile
+++ Dockerfile
@@ -63,7 +63,7 @@ COPY ansible/requirements.yml /ansible-requirements.yml
 COPY package*.json ./
 
 RUN python3 -m pip install -U pip && \
-    python3 -m pip install --upgrade netaddr awscli jmespath ansible ansible-lint boto3 botocore && \
+    python3 -m pip install --upgrade netaddr awscli jmespath ansible && \
     ansible-galaxy install -r /ansible-requirements.yml && \
     npm install
 
--- terraform/aws/security_groups.tf
+++ terraform/aws/security_groups.tf
@@ -320,18 +320,6 @@ resource "aws_security_group" "hp_masternode" {
     ])
   }
 
-  # ZeroSSL IP verification
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    description = "ZeroSSL IP verification"
-
-    cidr_blocks = [
-      "0.0.0.0/0",
-    ]
-  }
-
   tags = {
     Name        = "dn-${terraform.workspace}-hp-masternode"
     DashNetwork = terraform.workspace
@@ -454,23 +442,5 @@ resource "aws_security_group" "vpn" {
   tags = {
     Name        = "dn-${terraform.workspace}-vpn"
     DashNetwork = terraform.workspace
-  }
-}
-
-resource "aws_security_group" "seed" {
-  name        = "${terraform.workspace}-seed"
-  description = "DAPI access"
-  vpc_id      = aws_vpc.default.id
-
-  ingress {
-    description = "DAPI from internet"
-    from_port   = var.dapi_port
-    to_port     = var.dapi_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "seed-1"
   }
 }
